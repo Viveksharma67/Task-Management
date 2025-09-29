69188c01f027b69ee92ebf63803514af9ec6f226
Roshan-9 <viveksharma5429@gmail.com>
2025-09-29 13:54:14 +0530

    Initial commit of Task Management App

---
+++ client/src/App.jsx
@@ -0,0 +1,33 @@
+// App.jsx
+import { Routes, Route } from "react-router-dom";
+import Layout from "./components/Layout";
+import HomePage from "./pages/HomePage";
+import TaskListPage from "./pages/TaskListPage";
+import ShowTask from "./pages/ShowTask";
+import Signup from "./components/Signup";
+import Login from "./components/Login";
+
+export default function App() {
+  return (
+    
+    <Routes >
+<<<<<<< HEAD
+      
+      <Route path="/" element={<Signup />} />
+      <Route path="/login" element={<Login />} />
+
+  
+=======
+      <Route path="/" element={<Signup />} />
+      <Route path="/login" element={<Login />} />
+
+
+>>>>>>> 729d0a2 (Updated task feature / new changes)
+      <Route path="/app" element={<Layout />}>
+        <Route index element={<HomePage />} />
+        <Route path="task-list" element={<TaskListPage />} />
+        <Route path="show-task/:taskid" element={<ShowTask />} />
+      </Route>
+    </Routes>
+  );
+}
